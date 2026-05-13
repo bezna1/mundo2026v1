2c02c4937f6d385fefc98c1d81577e00093395df
bossboss <krolsiwek@gmail.com>
2026-05-13 12:57:52 +0200

    Add GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
+  base: '/mundial-typer/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    port: 3000,
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-  },
 })
